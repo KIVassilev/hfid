d1bb71c54f827a4e5d659bf8e2a58449a34cab80
kiv <kiv@e2p.io>
2020-12-14 18:26:25 +0200

    Add mapping T to 7

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 const hfid = require('.');
 const assert = require('assert');
 
-// Are we replacing B O and I
-assert.equal(hfid.fix('b8io'), '8810');
+// Are we replacing B,O, T and I
+assert.equal(hfid.fix('b8i1o0t7'), '88110077');
 // Are we uppercasing all
-assert.equal(hfid.fix('acdefghjklmnpqrstuvwxyz'), 'ACDEFGHJKLMNPQRSTUVWXYZ');
+assert.equal(hfid.fix('acdefghjklmnpqrsuvwxyz'), 'ACDEFGHJKLMNPQRSUVWXYZ');
 
 // Are we generating ids of required length
 assert.equal(hfid.random(6).length, 6);
