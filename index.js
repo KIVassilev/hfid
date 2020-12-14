2439be0c260478fec7a40020827df126d8cf2b39
kiv <kiv@e2p.io>
2020-12-14 18:50:00 +0200

    Add substitution of Q to 0

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'U', 'V', 'W', 'X', 'Y', 'Z']
-const subst = { O: '0', I: '1', B: '8', T: 7 };
+const alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'U', 'V', 'X', 'Y', 'Z']
+const subst = { O: '0', I: '1', B: '8', T: 7, Q: 0 };
 
 exports.fix = function(s) {
   let res = '';
